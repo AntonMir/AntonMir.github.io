cad4a6cd89f69abd1dc348d2539ca0669f48c3a1
Р34-Антон <r34-anton@monowork.pro>
2023-12-24 23:55:24 +0300

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+// //    "start": "HTTPS=true SSL_CRT_FILE=/Users/anton/DEV/myProjects/investamp-web/server.crt SSL_KEY_FILE=/Users/anton/DEV/myProjects/investamp-web/server.key PORT=5000 react-scripts start",
+
+
+function App() {
+  return (
+    <AppStyled>
+
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  
+`
+
+export default App;
